baa25ad484ce35c4182e1311cd8a5709c9dce8fc
jin young97 <wlsdudajtwu@gmail.com>
2023-05-19 14:14:49 +0900

    sleep init

---
+++ sleep/js/hello.js
@@ -0,0 +1,15 @@
+let myName = "장성호";
+
+const pi = 3.14;
+var num = 10;
+i = 10;
+console.log(104 + 231);
+for (let i = 0; i < 10; i++) {
+  console.log("hello js");
+}
+console.log(i);
+function add(a, b) {
+  return a + b;
+}
+const result = add(10, 20);
+console.log(result);
