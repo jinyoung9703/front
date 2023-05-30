b4d7661273c5004650e76e4da6a37e16c547f521
jin young97 <wlsdudajtwu@gmail.com>
2023-05-30 18:20:18 +0900

    img bt

--- javascript/corona/js/main.js
+++ javascript/corona/js/main.js
@@ -6,7 +6,6 @@
 // console.log("02");
 // console.log("04");
 //비동기 예시.
-const thumbsList = document.querySelector(".thumbs-box ul");
 
 const search = document.querySelector(".search");
 search.addEventListener("keyup", (e) => {
@@ -18,24 +17,124 @@ search.addEventListener("keyup", (e) => {
 
 //promise 홍대역 8번출구 맥도날드() fullfield 약속이 이행되면 / rejected 약속이 지켜지지 않음
 
-const searchImg = (searchWord) => {
-  const aa = fetch(`https://dapi.kakao.com/v2/search/image?query=${searchWord}`, {
-    headers: {
-      Authorization: "KakaoAK 5d7246f1af7602008f2529298ebdf6b5",
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      // console.log(data.meta.is_end);
-      console.log(data);
-      const imgList = data.documents;
-      imgList.forEach(function (item, idx) {
-        const li = document.createElement("li");
-        const img = document.createElement("img");
-        img.src = item.thumbnail_url;
-        li.append(img);
-        thumbsList.append(li);
-      });
+// const searchImg = (searchWord) => {
+//   const aa = fetch(`https://dapi.kakao.com/v2/search/image?query=${searchWord}`, {
+//     headers: {
+//       Authorization: "KakaoAK 5d7246f1af7602008f2529298ebdf6b5",
+//     },
+//   })
+//     .then((response) => response.json())
+//     .then((data) => {
+//       // console.log(data.meta.is_end);
+//       console.log(data);
+//       const imgList = data.documents;
+//       imgList.forEach(function (item, idx) {
+//         const li = document.createElement("li");
+//         const img = document.createElement("img");
+//         img.src = item.thumbnail_url;
+//         li.append(img);
+//         thumbsList.append(li);
+//       });
+//     });
+//   console.log(aa);
+// };
+
+// 이미지
+
+const imgbt = document.querySelector(".imgbt");
+const video = document.querySelector(".video");
+const thumbsList = document.querySelector(".thumbs-box ul");
+
+imgbt.addEventListener("click", function () {
+  const removeItem = () => {
+    const imgItem = document.querySelectorAll(".thumbs-box ul li");
+    imgItem.forEach((item, idx) => {
+      item.remove();
     });
-  console.log(aa);
-};
+  };
+
+  const searchImg = async (searchWord) => {
+    removeItem();
+
+    const imgResponse = await fetch(`https://dapi.kakao.com/v2/search/image?query=${searchWord}`, {
+      headers: {
+        Authorization: "KakaoAK 5d7246f1af7602008f2529298ebdf6b5",
+      },
+    });
+
+    const imgJson = await imgResponse.json();
+    const imgList = imgJson.documents;
+
+    imgList.forEach(function (item, idx) {
+      const li = document.createElement("li");
+      const img = document.createElement("img");
+      const a = document.createElement("a");
+
+      img.src = item.thumbnail_url;
+      a.href = item.image_url;
+      a.setAttribute("data-fancybox", "gallery");
+      a.append(img);
+      li.append(a);
+      thumbsList.append(li);
+    });
+
+    gsap.from(".thumbsList li", { scale: 0, duration: 1, stagger: 0.02 });
+
+    Fancybox.bind("[data-fancybox]", {
+      // 필요한 옵션 설정
+    });
+  };
+
+  searchImg(`https://dapi.kakao.com/v2/search/image?query=${searchWord}`); // 검색어를 원하는 키워드로 변경해주세요
+  alert("딸깍");
+});
+
+video.addEventListener("click", function () {
+  const removeItem = () => {
+    const imgItem = document.querySelectorAll(".thumbs-box ul li");
+    imgItem.forEach((item, idx) => {
+      item.remove();
+    });
+  };
+
+  const searchVideo = async (searchWord) => {
+    removeItem();
+
+    const imgResponse = await fetch(`https://dapi.kakao.com/v2/search/vclip?query=${searchWord}&size=30`, {
+      headers: {
+        Authorization: "KakaoAK 5d7246f1af7602008f2529298ebdf6b5",
+      },
+    });
+
+    const imgJson = await imgResponse.json();
+    const imgList = imgJson.documents;
+
+    imgList.forEach(function (item, idx) {
+      const li = document.createElement("li");
+      const img = document.createElement("img");
+      const a = document.createElement("a");
+
+      img.src = item.thumbnail;
+      a.href = item.url;
+      a.setAttribute("data-fancybox", "gallery");
+      a.append(img);
+      li.append(a);
+      thumbsList.append(li);
+    });
+
+    gsap.from(".thumbsList li", { scale: 0, duration: 1, stagger: 0.02 });
+
+    Fancybox.bind("[data-fancybox]", {
+      // 필요한 옵션 설정
+    });
+  };
+
+  const thumbsList = prompt("검색어를 입력하세요"); // 검색어 입력 받기
+
+  if (searchInput) {
+    searchVideo(searchInput);
+    alert("딸깍");
+  } else {
+    alert("검색어를 입력하지 않았습니다.");
+  }
+});
--- javascript/corona/js/promise.js
+++ javascript/corona/js/promise.js
@@ -1,11 +1,71 @@
+//async/await
+
+/*
 const myPromise = new Promise(function (resolve, rejaect) {
   setTimeout(function () {
-    resolve("fail");
+    resolve("success");
+    // resolve("fail");
   }, 1000);
 });
-myPromise.then(function (msg) {
-  console.log(msg);
-});
-myPromise.catch(function (msg) {
-  console.log(msg);
-});
+console.log(myPromise);
+myPromise
+  .then(function (msg) {
+    console.log(msg);
+  })
+
+  .catch(function (msg) {
+    console.log(msg);
+  })
+
+  .finally(function () {
+    console.log("이건 무조건 출력");
+    console.log(myPromise);
+  });*/
+
+const user = {
+  id: "jjy03",
+  name: "정진영",
+};
+
+function fatchUser() {
+  const url = "https://jsonplaceholder.typicode.com/users/1";
+  return fetch(url).then((response) => response.json());
+}
+
+// 비동기적 실행을 동기적으로 바꾼다.
+async function checkName() {
+  const user = await fatchUser();
+  console.log(user);
+  if (user.id === 1) {
+    console.log(user.name);
+  }
+}
+
+//async funtion 앞에 넣어주면 await 을
+//사용할 수 있는데 비동기를 동기식으로 바꿔준다
+//async안에서만 await를 사용 할 수 있다.
+
+fetch("https://jsonplaceholder.typicode.com/posts/1")
+  .then((response) => response.json())
+  .then((data) => data.userId)
+  .then(function (userId) {
+    fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
+      .then((response) => response.json())
+      .then(function (user) {
+        // console.log(user.name);
+      });
+  });
+
+async function fetchUserName(postId) {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`);
+  const post = await response.json();
+  const userId = post.userId;
+  const userResponse = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`);
+  const user = await userResponse.json();
+  return user.name;
+}
+console.log(
+  fetchUserName(1).then(function (name) {
+    console.log(name);
+  })
+);
